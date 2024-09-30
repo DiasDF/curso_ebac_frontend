422e83541557521c15115edbb34e059305c442c3
Diasdf <81680685+DiasDF@users.noreply.github.com>
2024-09-30 18:44:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -25,21 +25,3 @@ form.addEventListener('submit', function(e) {
       mensagemMsg.style.color = 'red';
    }
 });
-
-
-/*
-// Captura o formulário
-// console.log(form)
-// Mostra o formulário
-3) Crie uma validação no JavaScript, na qual o formulário será válido caso 
-o número B seja maior que o número A.
-
-   let soma = parseFloat(numeroA) + parseFloat(numeroB);
-   console.log(`${soma}`);
-*/
-
-// numeroA.addEventListener('keyup', function(e) {
-//    const AlteraA = parseFloat(e.target.value);
-//    console.log(`Valor A: ${AlteraA}`);
-// });
-
