bc0278c3cbe9d303ebe301f2314c20cc2696c6ca
Diasdf <81680685+DiasDF@users.noreply.github.com>
2024-09-30 18:16:01 -0300

    Update main.js

--- mod7.9/main.js
+++ mod7.9/main.js
@@ -18,29 +18,10 @@ form.addEventListener('submit', function(e) {
    formEValido = validaCampo(campoA, campoB)
    
    if(formEValido){
-      mensagemMsg.textContent = 'Ok número B é maior';
+      mensagemMsg.textContent = 'Ok: O Número B é maior';
       mensagemMsg.style.color = 'green';
    } else {
-      mensagemMsg.textContent = 'Erro: O número A é maior';
+      mensagemMsg.textContent = 'Erro: O Número A deve ser menor';
       mensagemMsg.style.color = 'red';
    }
 });
-
-
-/*
-// Captura o formulário
-// console.log(form)
-// Mostra o formulário
-3) Crie uma validação no JavaScript, na qual o formulário será válido caso 
-o número B seja maior que o número A.
-
-   let soma = parseFloat(numeroA) + parseFloat(numeroB);
-   console.log(`${soma}`);
-*/
-
-// numeroA.addEventListener('keyup', function(e) {
-//    const AlteraA = parseFloat(e.target.value);
-//    console.log(`Valor A: ${AlteraA}`);
-// });
-
-
